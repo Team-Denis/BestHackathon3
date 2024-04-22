dc697a91b5302b4a95fdf032a8d297e47d7ccc4e
Lucien Fiorini <lucienfiorini@gmail.com>
2024-04-22 21:12:23 +0200

    remove more abs path

--- aoc/script.js
+++ aoc/script.js
@@ -6,6 +6,6 @@ loginForm.addEventListener("submit", (e) => {
   let answer = document.getElementById("answer");
 
   if (md5(answer.value) === "3ba9af181751761d3b387f74ded2d783") {
-    document.getElementById("next").innerHTML = `Fragment de 🔥 [1/3] = ${answer.value}, <a href="/chess">passer à la suite</a>`;
+    document.getElementById("next").innerHTML = `Fragment de 🔥 [1/3] = ${answer.value}, <a href="./chess">passer à la suite</a>`;
   }
 });
